2e0abcd720845167e0d1401bc9e56479a877c077
Senanur <136026071+Senakurtnezir@users.noreply.github.com>
2024-05-19 22:12:47 +0300

    Delete urun_listeleme.js

--- urun_listeleme.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('urun_listeleme_button').addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    
-    window.location.href = 'urun_listeleme.html';
-});
