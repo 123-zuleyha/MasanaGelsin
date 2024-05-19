ec755a1399144da1d6e6749981d51d823d8f8aa1
Senanur <136026071+Senakurtnezir@users.noreply.github.com>
2024-05-19 22:12:22 +0300

    Delete urun_guncelleme.js

--- urun_guncelleme.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('urun_guncelle_button').addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    
-    window.location.href = 'urun_guncelleme.html';
-});
